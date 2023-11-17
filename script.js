e6dbbd8da3f62acd66a2fa472c8008d8dc5ecac2
Sadia Naaz <139625425+Sadia-Naaz@users.noreply.github.com>
2023-11-17 10:35:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 var dateDiffInDays = function (date1, date2) {
   //   write your code here
+	const parseDate1 = new Date(date1);
+	const parseDate2 = new Date(date2);
+	const timeDifference = parseDate2-parseDate1;
+	const daysDifference = Math.floor(timeDifference/(1000*24*60*60));
+	return daysDifference;
+
+	
 };
+	 
+	
+	
 
 // Do not change the code below.
 const dateOne = prompt("Enter Start Date.");
